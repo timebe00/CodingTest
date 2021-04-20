da8717ec2a0eed631da83e88fb119df298f42052
timebe00 <an0160@naver.com>
2021-04-21 01:17:08 +0900

    FindKim.py fix

--- Python/level1/FindKim/FindKim.py
+++ Python/level1/FindKim/FindKim.py
@@ -1,9 +1,7 @@
 #   서울에서 김서방 찾기
 
 def solution(seoul):
-    for i in range(len(seoul)):
-        if(seoul[i] == "Kim"):
-            return "김서방은 " + str(i) + "에 있다"
+    return "김서방은 " + str(seoul.index("Kim")) + "에 있다"
 
 seoul = ["Jane", "Kim", "Jane"]
 solution(seoul)
